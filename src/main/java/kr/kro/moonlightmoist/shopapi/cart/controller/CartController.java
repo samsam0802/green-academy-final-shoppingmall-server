3bfa52f02d8d37899cdae97594bf803a403fe63a
KangMinSuk <kmsk0802@gmail.com>
2025-11-24 17:39:43 +0900

    장바구니 기능 구현
    - @CrossOrigin 어노테이션 추가
    - 일부 코드 수정(@RequestBody 대신 @PathVariable 사용)

--- src/main/java/kr/kro/moonlightmoist/shopapi/cart/controller/CartController.java
+++ src/main/java/kr/kro/moonlightmoist/shopapi/cart/controller/CartController.java
@@ -5,7 +5,6 @@ import kr.kro.moonlightmoist.shopapi.cart.dto.CartProductListDTO;
 import kr.kro.moonlightmoist.shopapi.cart.service.CartService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,25 +12,33 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@CrossOrigin(origins = "*", allowedHeaders = "*",
+        methods = {RequestMethod.GET,
+                RequestMethod.POST,
+                RequestMethod.PUT,
+                RequestMethod.DELETE,
+                RequestMethod.OPTIONS
+               })
 @RequestMapping("/api/cart")
 public class CartController {
     private final CartService cartService;
 
     @PostMapping("/change")
     public List<CartProductListDTO> changeCart(@RequestBody CartProductDTO dto) {
+        log.info("changeCart dto => {}", dto);
         if(dto.getQuantity() <= 0) {
             return cartService.remove(dto.getId());
         }
         return cartService.addOrModify(dto);
     }
 
-    @GetMapping("/items")
-    public List<CartProductListDTO> getCartItems(@RequestBody Long userId) {
+    @GetMapping("/items/{userId}")
+    public List<CartProductListDTO> getCartItems(@PathVariable Long userId) {
         return cartService.getCartItems(userId);
     }
 
-    @DeleteMapping("/delete")
-    public List<CartProductListDTO> deleteCartItem(@RequestBody Long cartProductId){
+    @DeleteMapping("/{cartProductId}")
+    public List<CartProductListDTO> removeCartItem(@PathVariable Long cartProductId){
         return cartService.remove(cartProductId);
     }
 
